42916ed050ba8a93be92a6ddf5d0ba5c9b46be3b
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2023-07-10 10:53:57 -0300

    Adicionar animações da galeria

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 'use strict'
-import { animate } from  'motion'
+import { animate, scroll } from  'motion'
 
-console.log(animate)
+const wrapper = document.querySelector(".carousel-wrapper")
+
+const scrollOptions = {
+  target: wrapper,
+  offset: ["start -200px", "-200px end"]
+}
+
+scroll(
+  animate('.carousel[data-direction="left"]',{ x: -250 }),
+  scrollOptions
+)
+
+scroll(
+  animate('.carousel[data-direction="right"]',{ x: 250 }),
+  scrollOptions
+)
