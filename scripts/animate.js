cda0604224992c3cc99a4c7f4c03946f19b375bd
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2023-08-04 12:22:07 -0300

    adicionar animações, adicionar comando de build

--- scripts/animate.js
+++ scripts/animate.js
@@ -26,18 +26,43 @@ scroll(
 inView('[data-animate="opacity"]', ({target})=>{
   animate(
     target, 
-    {opacity: [0, 1]},
-    { delay: 0.2, duration: 0.9}
+    {y: ["-20px", 0], opacity: [0, 1], filter: ["blur(0.1rem)", 0]},
+    { delay: 0.3, duration: 0.5}
   )
 
-  return ({target}) => {
-    animate(
-      target,
-      {opacity: [1, 0]},
-      { delay: 0.2, duration: 0.9}
-    )
-  }
+  return ({target}) => {};
 })
+
+inView('[data-animate="turning"]', ({target})=>{
+  animate(
+    target, 
+    {rotateY: ["-30deg", 0], skewY: ["30deg", 0], opacity: [0, 1]},
+    { delay: 0.2, duration: 0.5}
+  )
+  return ({target}) => {};
+})
+
+const hoverElements = document.querySelectorAll('[data-animate="hover"]')
+
+hoverElements.forEach(el =>{
+  el.addEventListener('mouseover', ()=>{
+    animate(
+      el, 
+      {scale: [1, 0.95]}, 
+      {duration: 0.3, easing: "ease-in-out"}
+    );
+    el.classList.add("active");
+  })
+  el.addEventListener('mouseout', ()=>{
+    animate(
+      el, 
+      {scale: 1}, 
+      {duration: 0.3, easing: "ease-in-out"}
+    )
+    el.classList.remove("active");
+  })
+})
+
 
 const navOpenBtn = document.querySelector('.nav-button');
 const navCloseBtn = document.querySelector('.nav-button-close');
